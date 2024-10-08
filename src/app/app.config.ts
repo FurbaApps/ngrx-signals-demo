c8dc7ebbaf8b5c74de475da84a8a8e4cd555f96c
Igor Augustynski <igor.augustynski@furba.eu>
2024-10-08 21:03:57 +0200

    add @angular/material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,11 @@ import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZ
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // This provides change detection without Zone.js
     provideExperimentalZonelessChangeDetection(), 
-    provideRouter(routes)]
+    provideRouter(routes), provideAnimationsAsync()]
 };
